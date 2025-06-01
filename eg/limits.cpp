485be1df5b207952806747c73d2d3ad4fedc4110
AstroGeeKX <402878692@qq.com>
2025-06-01 16:35:00 +0800

    int等类型的最大范围宏示例

---
+++ eg/limits.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main() {
+    cout << "INT_MAX : " << INT_MAX << endl;
+    cout << "UINT_MAX : " << UINT_MAX << endl;
+    cout << "LONG_MAX : " << LONG_MAX << endl;
+    cout << "ULONG_MAX : " << ULONG_MAX << endl;
+    cout << "LLONG_MAX : " << LLONG_MAX << endl;
+    cout << "ULLONG_MAX : " << ULLONG_MAX << endl;
+    return 0;
+}
